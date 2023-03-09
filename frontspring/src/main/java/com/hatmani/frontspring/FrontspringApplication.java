096e8bcbb71cafb90ddecb5ad6edc7e3f6868626
hatkar <hatmani_karim@hotmail.com>
2023-03-09 20:39:19 +0100

    Creation de modules front spring and backend

---
+++ frontspring/src/main/java/com/hatmani/frontspring/FrontspringApplication.java
@@ -0,0 +1,13 @@
+package com.hatmani.frontspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontspringApplication.class, args);
+	}
+
+}
---
+++ frontspring/src/test/java/com/hatmani/frontspring/FrontspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatmani.frontspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
